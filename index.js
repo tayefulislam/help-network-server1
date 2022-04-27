18f30641569f129ee856eaa4766557d67f128576
tayeful912 <kazitayefulislam12@gmail.com>
2022-04-28 04:42:01 +0600

    connet to mongodb

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
-const app = express()
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 //middleware
 
 app.use(cors())
 app.use(express.json())
+
+
+
+// data base
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@helpnetwork.5hvks.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    await client.connect()
+
+    const eventCollections = client.db("helpNetwork").collection("events");
+
+    try {
+
+        console.log('hello')
+
+
+
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {

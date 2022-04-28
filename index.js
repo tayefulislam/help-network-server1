1fb345156788b88e40a85f9f961a1d2a0dec5a63
tayeful912 <kazitayefulislam12@gmail.com>
2022-04-28 17:41:44 +0600

    registration completed

--- index.js
+++ index.js
@@ -23,11 +23,48 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
 
-    await client.connect()
 
-    const eventCollections = client.db("helpNetwork").collection("events");
 
     try {
+
+        await client.connect()
+
+        const eventCollections = client.db("helpNetwork").collection("events");
+        const lImageCollections = client.db("helpNetwork").collection("landingData");
+
+        const volunteerCollections = client.db("helpNetwork").collection("volunteers");
+
+
+
+        // laanfing page img load
+
+        app.get('/imagedata', async (req, res) => {
+            const query = {}
+            const data = lImageCollections.find(query);
+            const result = await data.toArray()
+            res.send(result)
+        })
+
+        // add event
+        app.post('/addevent', async (req, res) => {
+
+            const event = req.body;
+
+            const result = await eventCollections.insertOne(event)
+            res.send(result)
+
+
+        })
+
+        // add volueteer
+
+        app.post('/addvolunteer', async (req, res) => {
+            const volunteer = req.body;
+            const result = await volunteerCollections.insertOne(volunteer);
+            res.send(result)
+        })
+
+
 
         console.log('hello')
 
